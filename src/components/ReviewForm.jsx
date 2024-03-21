3acc8e4072a0b021b411c1abc302ff85f641fdbe
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-03-21 00:45:28 -0300

    feat: development component ReviewForm(jsx and styles css)

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,7 +1,46 @@
+import {
+  BsFillEmojiHeartEyesFill,
+  BsEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsEmojiFrownFill,
+} from "react-icons/bs";
+
+import "./ReviewForm.css";
+
 const ReviewForm = () => {
   return (
-    <div>
-      <h2>ReviewForm</h2>
+    <div className="review-form">
+      <div className="form-control score-container">
+        <label className="radio-container">
+          <input type="radio" name="review" value="unsatisfied" required />
+          <BsEmojiFrownFill />
+          <p>Insatisfeito</p>
+        </label>
+        <label className="radio-container">
+          <input type="radio" name="review" value="neutral" required />
+          <BsFillEmojiNeutralFill />
+          <p>Poderia ser melhor</p>
+        </label>
+        <label className="radio-container">
+          <input type="radio" name="review" value="satisfied" required />
+          <BsEmojiSmileFill />
+          <p>Satisfeito</p>
+        </label>
+        <label className="radio-container">
+          <input type="radio" name="review" value="very_satisfied" required />
+          <BsFillEmojiHeartEyesFill />
+          <p>Muito satisfeito</p>
+        </label>
+      </div>
+      <div className="form-control">
+        <label htmlFor="comment">Comentário:</label>
+        <textarea
+          name="comment"
+          id="comment"
+          placeholder="Conte como foi a sua experiência com o produto..."
+          required
+        ></textarea>
+      </div>
     </div>
   );
 };
